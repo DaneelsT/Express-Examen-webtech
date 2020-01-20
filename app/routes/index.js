b7e3a95a8b5323050dc32e1987788f2719742ceb
DaneelsT <43535507+DaneelsT@users.noreply.github.com>
2020-01-20 09:01:45 +0100

    List of all students

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+const MongoClient = require('mongodb').MongoClient
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send("Hello World");
-});
+var db;
+
+MongoClient.connect('mongodb://localhost:27017/exam',
+  (err, database) => {
+    if (err) return console.log(err)
+    db = database.db('exam')
+    db.createCollection("students", function(err, res) {
+      if (err) throw err;
+      console.log("Collection created!");
+    })
+  })
+
+  router.get("/", (req,res) => {
+    db.collection('students').find().toArray((err, result) => {
+      if (err) return
+      res.render('list.ejs', {students: result})
+    })
+  })
 
 module.exports = router;
